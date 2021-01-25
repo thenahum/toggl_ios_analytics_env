3476d977dd7152c0a6b8deab44945f2fe8115758
thenahum <nahumgarcia@outlook.com>
2021-01-24 17:24:47 -0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -5,7 +5,7 @@ import base64
 ###### Toggl API Token
 token='<YOUR API TOKEN>'+':api_token'
 
-###### Define headers and paramaters for Toggl Request (documentation available here: https://github.com/toggl/toggl_api_docs)
+###### Define headers and paramaters for Toggl Request 
 headers={
     'Authorization':'Basic '+base64.b64encode(token.encode('ascii')).decode("utf-8")}
 params={'user_agent':'<YOUR EMAIL ADDRESS>','workspace_id':'<YOUR WORKSPACE ID>'}
